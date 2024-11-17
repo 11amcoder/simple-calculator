4c449a8ef18400fecc73782c56a6ffc792872583
N can <numaanbaig1996@gmail.com>
2024-11-17 11:22:59 +0530

    Update calc simple.py

--- calc simple.py
+++ calc simple.py
@@ -1,35 +1,36 @@
-print('-'*80)
-print("                                   CALCULATOR                                   ")
+print('-'*80) #draws hyphen for 80 character spaces length
+print("                                   CALCULATOR                                   ") #makes 10 characters of text "CALCULATOR" with 70 white spaces
 print("-"*80)
 
-while True:
+while True:   #this loop runs untill the user enters valid number
     try:
         user = float(input("Enter a number: ")) 
         op = input("Choose the operator: ['add','sub','div','mod','mul','xor','exp']:  ")
 
-        if op not in ("xor" , "add" , "exp" , "sub" , "div" , "mod" , "mul") or (user < 0 and op == "xor"):
+        if op not in ("xor" , "add" , "exp" , "sub" , "div" , "mod" , "mul") or (user < 0 and op == "xor"):  #if user enters spelling mistake or anyother thing apart from the selection, then it asks to enter a proper option from the following again.
             print("Invalid Operator! Please choose a valid operator from ['add','sub','div','mod','mul','xor','exp']: ")
-            continue  
+            continue         # and restart the loop again 
 
         numb = float(input("Enter another number: "))
 
-        if op == "xor" and (user <0 or numb < 0):
-            print("Please enter positive valid number for bitwise XOR operation: ")
-            continue
-        break
+        if op == "xor" and (user <0 or numb < 0):    #checks if operator is xor, and both the numbers are not negative
+            print("Please enter positive valid number for bitwise XOR operation: ") #if improper, shows warning
+            continue   #and restart the loop again
+        break  #if user enters correct number and opreator, then loop breaks
     
     except ValueError:
-        print("Please enter valid number:  ")
+        print("Please enter valid number:  ")   # in while loop, if user enters invalid number, it shows warning.
 
     
-user = int(user)
-numb = int(numb)
+user = int(user) #converting to int
+numb = int(numb) 
 
 
 
-usercopy = user
+usercopy = user #copying the original input to use it later
 numbcopy = numb
 
+# Block of arithmetic operations
 addition = user + numb
 subtraction = user - numb
 multiplication = user * numb
@@ -37,6 +38,7 @@ division = user/numb
 mod = user % numb
 exp = abs(user) ** abs(numb)
 
+#conditions for arithmetic operations
 if op == "add":
     print(addition)
 elif op == "sub":
@@ -50,16 +52,17 @@ elif op == "mod":
 elif op == "exp":
     print(exp)
 
+#bitwise XOR operation starts
 bin = []
 while True:
-    rem = int(user%2)
-    bin.append(rem)
-    user = int(user/2)
-    if user == 0:
+    rem = int(user%2)  #getting remainder of the user input
+    bin.append(rem)  #storing it somewhere to use it later
+    user = int(user/2) #updating user input by diviision
+    if user == 0:  #if user input has become 0, then the loop breaks as that's where we should stop while computing binary
         break
 
-bin.reverse()
-binary_string1 = ''.join(map(str,bin))
+bin.reverse() #reverse the list
+binary_string1 = ''.join(map(str,bin)) #convert it into strings
 
 
 bin2 = []
@@ -74,8 +77,8 @@ bin2.reverse()
 binary_string2 = ''.join(map(str,bin2))
 
 
-max_binary_length = max(len(binary_string1),len(binary_string2))
-binary_string1_padding = binary_string1.zfill(max_binary_length)
+max_binary_length = max(len(binary_string1),len(binary_string2))  #getting the maximum length of binary list
+binary_string1_padding = binary_string1.zfill(max_binary_length) #padding up the empty slots with 0's
 binary_string2_padding = binary_string2.zfill(max_binary_length)
 
 if op == "xor":
@@ -83,26 +86,26 @@ if op == "xor":
     print(f"Binary conversion for {numbcopy} is {binary_string2_padding}") #this is string
 
 
-newbitwisebin = []
+newbitwisebin = [] 
 
-for x1, x2 in zip(binary_string1_padding,binary_string2_padding):
-    x1 = int(x1)
+for x1, x2 in zip(binary_string1_padding,binary_string2_padding): #iterate through both the binary lists 
+    x1 = int(x1)   #making them convert to integer from str
     x2 = int(x2)
 
-    if x1 == x2:
+    if x1 == x2:       #implementing the xor truth table logic, if both values have 0 or 1, then it will be 0
         newbitwisebin.append(0)
-    elif x1 != x2:
+    elif x1 != x2:     #if both values have different values, then it will be 1
         newbitwisebin.append(1)
 
 
 
+# Binary to number conversion
+decimal_number = 0 
+length = len(newbitwisebin) #calculating the length of binary number of xor
 
-decimal_number = 0
-length = len(newbitwisebin)
-
-
+#iterates through all the elements of binary number
 for x in range(length):
-    decimal_number += newbitwisebin[x] * (2 ** (length-1-x))
-if op == "xor":
-    print(f"Based on XOR Truth Table, binary number for {binary_string1_padding} and {binary_string2_padding} is {newbitwisebin}")
-    print(f"After the XOR operation, decimal number which is equal to {newbitwisebin} is {decimal_number}")
+    decimal_number += newbitwisebin[x] * (2 ** (length-1-x))  # each element of binary list multiplied by 2**0,1,... (i.e., calculates the powe of 2 for each current element of the binary list}
+if op == "xor":       #if user selects the  operator as xor , then 
+    print(f"Based on XOR Truth Table, binary number for {binary_string1_padding} and {binary_string2_padding} is {newbitwisebin}") #prints binary number according to xor
+    print(f"After the XOR operation, decimal number which is equal to {newbitwisebin} is {decimal_number}") #prints decimal connversion of that xor binary number
